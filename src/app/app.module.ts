440405afe69a5dca6f0e048a943f4875e81d937a
faustmann <faustmann@gmail.com>
2020-01-13 16:43:53 +0100

    stopped at https://angular.io/tutorial/toh-pt6#add-heroserviceupdatehero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
+import { AppRoutingModule }     from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeroesComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, 
+  FormsModule,
+  HttpClientModule, 
+  AppRoutingModule, 
+  // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+  // and returns simulated server responses.
+  // Remove it when a real server is ready to receive requests.
+  HttpClientInMemoryWebApiModule.forRoot(
+    InMemoryDataService, { dataEncapsulation: false }
+  ) ],
+  declarations: [ AppComponent, HelloComponent, HeroesComponent, MessagesComponent, DashboardComponent, HeroDetailComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [HeroService, MessageService, InMemoryDataService]
 })
 export class AppModule { }
